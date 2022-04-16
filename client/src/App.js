f5f3db9747af910ef09d018a753eb66d9d6e5b81
Nisarg Dave <extrastuff58@yahoo.com>
2022-04-16 18:00:10 +0800

    Completed server and started working on frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,60 @@
-import React from 'react';
-import { Box, Grid } from '@chakra-ui/react';
+import React, { useState, useEffect, useRef } from 'react';
+import { Box, Grid, Button, Input } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
+import io from 'socket.io-client';
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState('');
+  const [yourID, setYourID] = useState();
+  // useRef hook allows you to persist values between renders and returns an object called current
+  const socketRef = useRef();
+  useEffect(() => {
+    socketRef.current = io.connect('/');
+    socketRef.current.on('your id', id => {
+      setYourID(id);
+    });
+    socketRef.current.on('message', message => {
+      handleMessages(message);
+    });
+  }, []);
+
+  const handleMessages = message => {
+    setMessages(oldMsgs => [...oldMsgs, message]);
+  };
+
+  const sendMessage = e => {
+    e.preventDefault();
+    const messageObj = {
+      id: yourID,
+      body: message,
+    };
+    setMessage('');
+    socketRef.current.emit('chat message', messageObj);
+  };
+
+  // Used with the on change event which detects when the value of an input element changes
+  const handleChange = e => {
+    setMessage(e.target.value);
+  };
   return (
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
+        {messages.map((message, index) => {
+          if (message.id === yourID) {
+            return <p>{message.body}</p>;
+          }
+          <p>{message.body}</p>;
+        })}
+        <form onSubmit={sendMessage}>
+          <Input
+            placeholder="Enter message"
+            width="auto"
+            onChange={handleChange}
+          />
+          <Button colorScheme="blackAlpha">Send</Button>
+        </form>
       </Grid>
     </Box>
   );
--- server/index.js
+++ server/index.js
@@ -4,8 +4,24 @@ const app = express();
 const http = require("http");
 // Server is going to be express application
 const server = http.createServer(app);
-
+const { Server } = require("socket.io");
+// Mount socket server to express app. IO is the server instance
+const io = new Server(server);
 app.get("/", (req, res) => {});
+
+io.on("connection", () => {
+  console.log("a user connected");
+  // Each client will communicate with the server (IO) through a socket with a unique id
+  socket.emit("your id", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("use disconnected");
+  });
+  socket.on("chat message", (msg) => {
+    // Emit to all users
+    io.emit("message", msg);
+  });
+});
 
 server.listen(4000, () => {
   console.log("Now listening on port 4000");
